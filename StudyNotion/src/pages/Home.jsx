55cbbcb22454f474190d0586622335108e964a59
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2023-12-16 21:20:35 +0530

    some file updated by false

--- StudyNotion/src/pages/Home.jsx
+++ StudyNotion/src/pages/Home.jsx
@@ -115,6 +115,25 @@ function Home() {
       </div>
 
       {/* section 2 */}
+      <div className="bg-pure-greys-5 text-richblue-700">
+        <div className="homepage_bg h-[333px]">
+
+          <div className="w-11/12 max-w-maxContent flex items-center gap-5 mx-auto">
+            <div className="flex flex-row gap-7 text-white">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-3">
+                  Explore Full Catalog
+                <FaArrowRight/>
+                </div>
+              </CTAButton>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
 
       {/* section 3 */}
 
