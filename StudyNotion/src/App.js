47ba614839b23f5426e36b958a80532758e21cd6
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2023-12-21 22:15:37 +0530

    navbar updated

--- StudyNotion/src/App.js
+++ StudyNotion/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+// import React from "react";
+import React, { Component }  from 'react';
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
